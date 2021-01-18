ef7e1dc42e6d5ab8fae317b91628463f8551591f
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-01-18 19:49:10 +0200

    Finally example

--- js/catch-error.js
+++ js/catch-error.js
@@ -1,16 +1,25 @@
-let json = `{ "age": 34, "name": "Dmytro" }`;
+let json = `{ "age": 34, "name: "Dmytro" }`;
 
-try {
-  //alert("Hello World!!!');
-  //eval(`alert("Hello World!!!')`);
+let fn = () => {
+  try {
+    //alert("Hello World!!!');
+    //eval(`alert("Hello World!!!')`);
 
-  let data = JSON.parse(json);
-  let name = data.name;
-  console.log(name);
+    let data = JSON.parse(json);
+    let name = data.name;
+    console.log(name);
 
-} catch (error) {
-  console.log(error.name);
-  console.log(error.message);
+  } catch (error) {
+    console.log(error);
+    console.log(error.name);
+    console.log(error.message);
+
+    return;
+  } finally {
+    console.log("JSON was processed!");
+  }
+
+  //console.log("JSON was processed!");
 }
 
-console.log("Finished");
+fn();
